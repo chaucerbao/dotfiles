f6f07a3be676863e14912cd8bfe361a802bc1815
Alvin Chan <aschan@gmail.com>
2014-08-28 10:48:22 -0700

    [Mjolnir] Add hotkey to resize to quarter screen

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -17,45 +17,63 @@ end
 -- Move the focused window to different locations
 function moveTo(target)
 	local focused = focused()
+	local window = focused.window
+	local screen = focused.screen
 	local x, y, toScreen
 
 	if target == 1 then
 		-- Quadrant 1 (Top-left)
-		x = focused.screen.x
-		y = focused.screen.y
+		x = screen.x
+		y = screen.y
 	elseif target == 2 then
 		-- Quadrant 2 (Top-right)
-		x = focused.screen.x + focused.screen.w - focused.window.w
-		y = focused.screen.y
+		x = screen.x + screen.w - window.w
+		y = screen.y
 	elseif target == 3 then
 		-- Quadrant 3 (Bottom-right)
-		x = focused.screen.x + focused.screen.w - focused.window.w
-		y = focused.screen.y + focused.screen.h - focused.window.h
+		x = screen.x + screen.w - window.w
+		y = screen.y + screen.h - window.h
 	elseif target == 4 then
 		-- Quadrant 4 (Bottom-left)
-		x = focused.screen.x
-		y = focused.screen.y + focused.screen.h - focused.window.h
+		x = screen.x
+		y = screen.y + screen.h - window.h
 	elseif target == 'center' then
 		-- Center
-		x = focused.screen.x + (focused.screen.w - focused.window.w) / 2
-		y = focused.screen.y + (focused.screen.h - focused.window.h) / 2
+		x = screen.x + (screen.w - window.w) / 2
+		y = screen.y + (screen.h - window.h) / 2
 	elseif target == 'nextScreen' or target == 'previousScreen' then
 		-- Previous/next screen
 		if target == 'nextScreen' then toScreen = focused.screenObject:next():frame_without_dock_or_menu()
 		else toScreen = focused.screenObject:previous():frame_without_dock_or_menu() end
-		x = toScreen.x + (focused.window.x - focused.screen.x) / focused.screen.w * toScreen.w
-		y = toScreen.y + (focused.window.y - focused.screen.y) / focused.screen.h * toScreen.h
+		x = toScreen.x + (window.x - screen.x) / screen.w * toScreen.w
+		y = toScreen.y + (window.y - screen.y) / screen.h * toScreen.h
 	end
 
 	focused.windowObject:settopleft({x = x, y = y})
 end
 
--- Expand the focused window to fit the entire screen
-function fullscreen()
+-- Resize the focused window to preset sizes
+function resizeTo(target)
 	local focused = focused()
+	local window = focused.window
 	local screen = focused.screen
+	local x, y, w, h
 
-	focused.windowObject:setframe({x = screen.x, y = screen.y, w = screen.w, h = screen.h})
+	if target == 'full' then
+		x = screen.x
+		y = screen.y
+		w = screen.w
+		h = screen.h
+	elseif target == 'quarter' then
+		w = screen.w / 2
+		h = screen.h / 2
+		if (window.x - screen.x + w > screen.w) then x = screen.x + screen.w - w
+		else x = window.x end
+		if (window.y - screen.y + h > screen.h) then y = screen.y + screen.h - h
+		else y = window.y end
+	end
+
+	focused.windowObject:setframe({x = x, y = y, w = w, h = h})
 end
 
 -- Resize the focused window, snapping to an invisible 8x8 grid
@@ -100,7 +118,8 @@ hotkey.bind(mods, "RIGHT", function() snapResize('w', 1) end)
 hotkey.bind(mods, "LEFT", function() snapResize('w', -1) end)
 hotkey.bind(mods, "DOWN", function() snapResize('h', 1) end)
 hotkey.bind(mods, "UP", function() snapResize('h', -1) end)
-hotkey.bind(mods, "F", fullscreen)
+hotkey.bind(mods, "F", function() resizeTo('full') end)
+hotkey.bind(mods, "Q", function() resizeTo('quarter') end)
 hotkey.bind(mods, "R", core.reload)
 
 print "Config successfully loaded"
