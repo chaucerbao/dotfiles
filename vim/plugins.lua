6cf1b60f6a0f3509badbe2a5c9e856821787f900
Alvin Chan <aschan@gmail.com>
2023-09-21 12:02:44 -0700

    [Vim] Reorganize plugin sequencing

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -17,40 +17,106 @@ require('packer').startup(function(use)
     branch = 'dev-v3',
     requires = {
       { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-nvim-lsp' },
+
       {
-        'zbirenbaum/copilot.lua',
-        cmd = 'Copilot',
-        event = 'InsertEnter',
+        'hrsh7th/nvim-cmp',
+        requires = {
+          { 'hrsh7th/cmp-buffer' },
+          { 'hrsh7th/cmp-nvim-lsp' },
+
+          {
+            'zbirenbaum/copilot-cmp',
+            requires = {
+              {
+                'zbirenbaum/copilot.lua',
+                cmd = 'Copilot',
+                event = 'InsertEnter',
+                config = function()
+                  require('copilot').setup({
+                    panel = { enabled = false },
+                    suggestion = { enabled = false },
+                  })
+                end,
+              },
+            },
+            after = { 'copilot.lua' },
+            config = function()
+              require('copilot_cmp').setup()
+            end,
+          },
+
+          {
+            'saadparwaiz1/cmp_luasnip',
+            requires = {
+              {
+                'L3MON4D3/LuaSnip',
+                config = function()
+                  require('luasnip.loaders.from_vscode').lazy_load()
+                end,
+              },
+              { 'rafamadriz/friendly-snippets' },
+            },
+          },
+        },
+
         config = function()
-          require('copilot').setup({
-            panel = { enabled = false },
-            suggestion = { enabled = false },
+          local cmp = require('cmp')
+          local luasnip = require('luasnip')
+          local has_words_before = function()
+            local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+          end
+
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                luasnip.lsp_expand(args.body)
+              end,
+            },
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'luasnip' },
+            }, {
+              {
+                name = 'buffer',
+                option = {
+                  get_bufnrs = function()
+                    return vim.api.nvim_list_bufs()
+                  end,
+                },
+              },
+              { name = 'copilot' },
+            }),
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_locally_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+              ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+            }),
           })
         end,
       },
-      {
-        'zbirenbaum/copilot-cmp',
-        after = { 'copilot.lua' },
-        config = function()
-          require('copilot_cmp').setup()
-        end,
-      },
-      {
-        'saadparwaiz1/cmp_luasnip',
-        requires = {
-          { 'L3MON4D3/LuaSnip' },
-          { 'rafamadriz/friendly-snippets' },
-        },
-        config = function()
-          require('luasnip.loaders.from_vscode').lazy_load()
-        end,
-      },
     },
+
     config = function()
       local lsp_zero = require('lsp-zero')
 
@@ -66,63 +132,22 @@ require('packer').startup(function(use)
           end, { buffer = bufnr })
         end
       end)
+    end,
+  })
 
-      require('mason').setup()
-      require('mason-lspconfig').setup({ handlers = { lsp_zero.default_setup } })
-
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local has_words_before = function()
-        local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, {
-          {
-            name = 'buffer',
-            option = {
-              get_bufnrs = function()
-                return vim.api.nvim_list_bufs()
-              end,
-            },
-          },
-          { name = 'copilot' },
-        }),
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-      })
+  use({
+    'williamboman/mason-lspconfig.nvim',
+    requires = {
+      {
+        'williamboman/mason.nvim',
+        config = function()
+          require('mason').setup()
+        end,
+      },
+    },
+    after = { 'lsp-zero.nvim' },
+    config = function()
+      require('mason-lspconfig').setup({ handlers = { require('lsp-zero').default_setup } })
     end,
   })
 
