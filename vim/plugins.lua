84fd0458ff8d0ee771154d6b85208d95f6b4247a
Alvin Chan <aschan@gmail.com>
2023-09-22 23:16:11 -0700

    [Vim] Prioritize LuaSnip source, add `<C-y>` completion mapping

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -73,14 +73,15 @@ require('packer').startup(function(use)
               end,
             },
             sources = cmp.config.sources({
-              { name = 'nvim_lsp' },
               { name = 'luasnip' },
+              { name = 'nvim_lsp' },
             }, {
               { name = 'copilot' },
             }),
             mapping = cmp.mapping.preset.insert({
               ['<C-b>'] = cmp.mapping.scroll_docs(-4),
               ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-y>'] = cmp.mapping.confirm({ select = true }),
               ['<Tab>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                   cmp.select_next_item()
