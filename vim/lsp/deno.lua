04120dbc9f16363d37fd56b3474384480f3a1749
Alvin Chan <aschan@gmail.com>
2025-06-04 22:59:15 -0700

    [Vim] Detect `root_dir` to differentiate between `deno` and `typescript-language-server`

--- vim/lsp/deno.lua
+++ vim/lsp/deno.lua
@@ -2,4 +2,11 @@ return {
   cmd = { 'deno', 'lsp' },
   filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
   root_markers = { 'deno.json', 'deno.jsonc' },
+  root_dir = function(_bufnr, on_dir)
+    local path = vim.fs.root(0, { 'deno.json', 'deno.jsonc' })
+
+    if path ~= nil then
+      on_dir(path)
+    end
+  end,
 }
--- vim/lsp/typescript-language-server.lua
+++ vim/lsp/typescript-language-server.lua
@@ -2,4 +2,11 @@ return {
   cmd = { 'typescript-language-server', '--stdio' },
   filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
   root_markers = { 'tsconfig.json', 'jsconfig.json' },
+  root_dir = function(_bufnr, on_dir)
+    local path = vim.fs.root(0, { 'tsconfig.json', 'jsconfig.json' })
+
+    if path ~= nil then
+      on_dir(path)
+    end
+  end,
 }
