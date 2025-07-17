7035a1e8ac82b7b9ba0c66de18d3b1dc2658c963
Alvin Chan <aschan@gmail.com>
2025-07-17 12:30:07 -0700

    [Vim] Don't attaching multiple `BufWritePre` events handlers when saving

--- vim/init.lua
+++ vim/init.lua
@@ -79,6 +79,7 @@ MiniDeps.now(function()
           end, { buffer = event.buf })
 
           vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+            group = vim.api.nvim_create_augroup('FormatOnSave', { clear = true }),
             callback = function()
               vim.lsp.buf.format({ name = 'efm-langserver' })
             end,
