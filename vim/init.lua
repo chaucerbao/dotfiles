59609186a023aa375d567863b73c7936c9f352e4
Alvin Chan <aschan@gmail.com>
2025-10-17 23:06:21 -0700

    [Vim] Update `mini.nvim` repository path

--- vim/init.lua
+++ vim/init.lua
@@ -1,7 +1,7 @@
 -- Package Manager
 local mini_path = vim.fn.stdpath('data') .. '/site/pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path })
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-mini/mini.nvim', mini_path })
   vim.cmd('packadd mini.nvim | helptags ALL')
 end
 
