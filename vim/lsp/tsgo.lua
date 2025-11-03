9ab8a352442d664e165a651cd244d11317b96af8
Alvin Chan <aschan@gmail.com>
2025-11-03 14:47:14 -0800

    [Vim] Add `tsgo` LSP

---
+++ vim/lsp/tsgo.lua
@@ -0,0 +1,18 @@
+local root_markers = { 'jsconfig.json', 'tsconfig.json' }
+
+return {
+  cmd = { 'tsgo', '--lsp', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+  root_dir = function(_bufnr, on_dir)
+    local path = vim.fs.root(0, root_markers)
+
+    if path ~= nil then
+      on_dir(path)
+    end
+  end,
+}
